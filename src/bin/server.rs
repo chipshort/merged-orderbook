e904b0aa0642b872df127fb0a46d930df34c1bb6
Christoph Otter <chipshort@tutanota.com>
2022-04-08 23:58:05 +0200

    Merge orderbooks

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,3 @@
-use async_stream::stream;
 use async_trait::async_trait;
 use clap::Parser;
 
@@ -6,13 +5,20 @@ use merged_orderbook::api;
 use merged_orderbook::api::orderbook_aggregator_server::{
     OrderbookAggregator, OrderbookAggregatorServer,
 };
+use merged_orderbook::exchange::binance::start_binance_task;
+use merged_orderbook::exchange::exchange_order_book::ExchangeOrderBook;
+use merged_orderbook::orderbook::{calculate_spread, merge};
 use std::net::SocketAddr;
 use std::{error::Error, pin::Pin};
 use tokio::select;
+use tokio::sync::watch;
+use tokio_stream::wrappers::WatchStream;
 use tokio_stream::{Stream, StreamExt};
 use tonic::{transport::Server, Status};
 
-struct Aggregator;
+struct Aggregator {
+    receiver: watch::Receiver<Option<api::Summary>>,
+}
 
 type ResponseStream = Pin<Box<dyn Stream<Item = Result<api::Summary, Status>> + Send>>;
 
@@ -24,58 +30,87 @@ impl OrderbookAggregator for Aggregator {
         &self,
         _request: tonic::Request<api::Empty>,
     ) -> Result<tonic::Response<Self::BookSummaryStream>, tonic::Status> {
-        // TODO: actually return correct stream
-        // let stream = Box::pin(tokio_stream::pending::<Result<api::Summary, _>>());
-
-        let mut s1 = Box::pin(stream! {
-            let mut i = 0f64;
-            loop {
-                yield i;
-                i += 1f64;
-            }
-        });
-        let mut s2 = Box::pin(stream! {
-            let mut i = 0f64;
-            loop {
-                yield i;
-                i -= 1f64;
-            }
-        });
-
-        let combined = stream! {
-            loop {
-                select! {
-                    Some(item) = s1.next() => {
-                        yield Ok(api::Summary {
-                            spread: item,
-                            asks: vec![],
-                            bids: vec![]
-                        });
-                    },
-                    Some(item) = s2.next() => {
-                        yield Ok(api::Summary {
-                            spread: item,
-                            asks: vec![],
-                            bids: vec![]
-                        });
-                    }
-                }
-            }
-        };
-
-        Ok(tonic::Response::new(Box::pin(combined)))
+        // every listener gets their own receiver
+        let receiver = self.receiver.clone();
+        let stream = WatchStream::new(receiver)
+            .filter_map(|orderbook| orderbook)
+            .map(|orderbook| Ok(orderbook));
+        Ok(tonic::Response::new(Box::pin(stream)))
+        // Err(tonic::Status::aborted(""))
     }
+}
+
+/// Spawns a task that merges the exchange orderbooks from two receivers and sends the result to the given sender.
+fn start_merge_task(
+    mut exchange0: watch::Receiver<Option<ExchangeOrderBook>>,
+    mut exchange1: watch::Receiver<Option<ExchangeOrderBook>>,
+    sender: watch::Sender<Option<api::Summary>>,
+) {
+    tokio::spawn(async move {
+        loop {
+            // wait for at least one of the receivers to get a new value
+            select! {
+                _ = exchange0.changed() => {}, // TODO: handle result, log error
+                _ = exchange1.changed() => {}
+            };
+
+            let orderbook0 = exchange0.borrow(); // TODO: borrow_and_update?
+            let orderbook1 = exchange1.borrow();
+
+            // get asks and bids of both orderbook
+            let asks0 = orderbook0.as_ref().map(|o| o.asks()).unwrap_or(&[]);
+            let bids0 = orderbook0.as_ref().map(|o| o.bids()).unwrap_or(&[]);
+            let asks1 = orderbook1.as_ref().map(|o| o.asks()).unwrap_or(&[]);
+            let bids1 = orderbook1.as_ref().map(|o| o.bids()).unwrap_or(&[]);
+
+            // extract top 10 asks / bids
+            let asks = merge(
+                asks0,
+                asks1,
+                |a1, a2| a1.price < a2.price, // asks are ordered from low to high
+                10,
+            );
+            let bids = merge(
+                bids0,
+                bids1,
+                |b1, b2| b1.price > b2.price, // bids from high to low
+                10,
+            );
+            let spread = calculate_spread(&asks, &bids);
+
+            sender
+                .send(Some(api::Summary {
+                    asks,
+                    bids,
+                    spread: spread.unwrap_or(f64::MAX),
+                }))
+                .expect("merge receiver should never be dropped");
+        }
+    });
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // setup cli
     let args: Cli = Cli::parse();
     let addr = args
         .listen
         .unwrap_or_else(|| "[::1]:50051".parse().unwrap());
 
-    let aggregator = Aggregator;
+    // start exchange websocket tasks
+    let (binance_sender, binance_receiver) = watch::channel(None);
+    let (bitstamp_sender, bitstamp_receiver) = watch::channel(None);
+    start_binance_task(args.symbol.clone(), binance_sender);
+    start_binance_task(args.symbol.clone(), bitstamp_sender); // TODO: implement bitstamp task instead
 
+    // start task for merging both exchange orderbooks
+    let (orderbook_sender, orderbook_receiver) = watch::channel(None);
+    start_merge_task(binance_receiver, bitstamp_receiver, orderbook_sender);
+
+    // start gRPC server
+    let aggregator = Aggregator {
+        receiver: orderbook_receiver,
+    };
     println!("Starting gRPC Server...");
     Server::builder()
         .add_service(OrderbookAggregatorServer::new(aggregator))
---
+++ src/orderbook.rs
@@ -0,0 +1,132 @@
+use crate::api::Level;
+
+/// Merges two already sorted lists, keeping only `first_n` items.
+/// `less_than` can be a strict order (<) or a partial order (<=).
+/// ```rust
+/// assert_eq!(merged_orderbook::orderbook::merge(&[0, 1, 6, 8], &[2, 3, 7], |a, b| a < b, 10), vec![0, 1, 2, 3, 6, 7, 8]);
+/// ```
+pub fn merge<T: Clone>(
+    a: &[T],
+    b: &[T],
+    less_than: impl Fn(&T, &T) -> bool,
+    first_n: usize,
+) -> Vec<T> {
+    // handle empty inputs
+    if a.len() == 0 {
+        return b.to_vec();
+    }
+    if b.len() == 0 {
+        return a.to_vec();
+    }
+
+    let mut i = 0;
+    let mut j = 0;
+    let mut sorted = Vec::with_capacity(first_n);
+
+    loop {
+        if sorted.len() == first_n {
+            // only interested in the top elements, so stop merging after we have them
+            break;
+        }
+        if less_than(&a[i], &b[j]) {
+            sorted.push(a[i].clone());
+            i += 1;
+            if i == a.len() {
+                // done with a, push rest of b to the end
+                let remaining_end = (j + first_n - sorted.len()).min(b.len());
+                if remaining_end < j {
+                    // no remaining elements
+                    break;
+                }
+                let rest = &b[j..remaining_end];
+                for elem in rest {
+                    sorted.push(elem.clone());
+                }
+                break;
+            }
+        } else {
+            sorted.push(b[j].clone());
+            j += 1;
+            if j == b.len() {
+                // done with b, push the rest of a to the end
+                let remaining_end = (i + first_n - sorted.len()).min(a.len());
+                if remaining_end < i {
+                    // no remaining elements
+                    break;
+                }
+                let remaining = &a[i..remaining_end];
+                for elem in remaining {
+                    sorted.push(elem.clone());
+                }
+                break;
+            }
+        }
+    }
+
+    sorted
+}
+
+pub fn calculate_spread(asks: &[Level], bids: &[Level]) -> Option<f64> {
+    let best_ask = asks.first()?.price;
+    let best_bid = bids.first()?.price;
+
+    Some(best_ask - best_bid)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::orderbook::merge;
+
+    #[test]
+    fn empty_remaining() {
+        assert_eq!(
+            merge(&[0, 2, 4, 6], &[1, 3, 5, 7], |a, b| a < b, 8),
+            vec![0, 1, 2, 3, 4, 5, 6, 7]
+        );
+    }
+
+    #[test]
+    fn correct_length() {
+        assert_eq!(
+            merge(&[0, 2], &[3, 6, 8], |a, b| a < b, 8),
+            vec![0, 2, 3, 6, 8]
+        );
+
+        assert_eq!(
+            merge(&[0, 2, 8], &[3, 6, 7, 9, 27], |a, b| a < b, 5),
+            vec![0, 2, 3, 6, 7]
+        );
+
+        assert_eq!(
+            merge(&[0, 6, 8], &[4, 6, 7, 9, 27], |a, b| a < b, 2),
+            vec![0, 4]
+        );
+    }
+
+    #[test]
+    fn generate_many() {
+        let a = vec![
+            2, 4, 6, 7, 8, 9, 12, 36, 57, 90, 123, 4576, 5786, 6789, 9809,
+        ];
+        let b = vec![
+            3, 4, 9, 10, 76, 87, 97, 99, 123, 156, 257, 546, 587, 690, 852, 946, 1798,
+        ];
+
+        for i in 0..a.len() {
+            let a = &a[0..i];
+            for j in 0..b.len() {
+                let b = &b[0..j];
+
+                let merged = merge(a, b, |a, b| a < b, a.len() + b.len());
+                for (item, next) in merged.iter().zip(merged.iter().skip(1)) {
+                    assert!(item <= next);
+                }
+
+                let merged = merge(b, a, |a, b| a < b, a.len() + b.len());
+                for (item, next) in merged.iter().zip(merged.iter().skip(1)) {
+                    assert!(item <= next);
+                }
+            }
+        }
+    }
+}
